3b4e6d7c7e3e5add32ec81a8b4d349d5dde8326f
Ibrahim Diba <ifraibrahim60@gmail.com>
2023-09-25 21:12:17 +0300

    Initial Commit

---
+++ src/main/java/com/ibrahim/inventorymanager/InventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahim.inventorymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibrahim/inventorymanager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.inventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
